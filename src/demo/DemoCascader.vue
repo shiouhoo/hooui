07a3bdcd50db2aee004c758314e978fc3a02b3a7
shiouhoo <shiouhoo@foxmail.com>
2024-03-18 18:21:05 +0800

    feat: 初步建立cascader组件文件

---
+++ src/demo/DemoCascader.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import CustomItem from '../package/cascader/CustomItem.vue';
+import BaseTab from './BaseTab.vue';
+import { message } from 'ant-design-vue';
+
+</script>
+<template>
+    <div class="wrapper">
+        <BaseTab class="h-100%">
+            <template #common>
+                <CustomItem class="m-10px"></CustomItem>
+            </template>
+            <template #unocss>
+                <CustomItem class="m-10px"></CustomItem>
+            </template>
+        </BaseTab>
+    </div>
+</template>
+
+<style scoped lang="less">
+.wrapper{
+  height: 500px;
+}
+</style>
---
+++ src/package/cascader/RootNav.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="nav-menu flex flex-col w-10rem overflow-auto py-5px">
+        <div v-for="(item,index) in treeData" :key="index" class="nav-item h-2rem flex items-center justify-between px-0.35rem flex-shrink-0 cursor-pointer hover:bg-#f5f5f5" :class="{'bg-#e6f7ff!':select === item.label}" @click="clickSelectItem(item.label)">
+            <div class="flex flex-center cursor-pointer">
+                <span class="ml-0.5rem -translate-y-0.08rem">{{ item.label }}</span>
+            </div>
+            <!-- 向右箭头 -->
+            <svg class="w-1rme h-1rem" focusable="false" data-icon="right" width="1em" height="1em" fill="currentColor" aria-hidden="true" viewBox="64 64 896 896"><path d="M765.7 486.8L314.9 134.7A7.97 7.97 0 00302 141v77.3c0 4.9 2.3 9.6 6.1 12.6l360 281.1-360 281.1c-3.9 3-6.1 7.7-6.1 12.6V883c0 6.7 7.7 10.4 12.9 6.3l450.8-352.1a31.96 31.96 0 000-50.4z"></path></svg>
+        </div>
+    </div>
+    <RootNav v-if="existNext" :tree-data="nextTreeData" :index="props.index+1"></RootNav>
+</template>
+
+<script lang='ts' setup>
+import RootNav from './RootNav.vue';
+
+const props = withDefaults(defineProps<{
+    treeData: Record<string, any>[];
+    index?: number;
+}>(), {
+    index: 0,
+});
+
+const nextTreeData = ref<Record<string, any>[]>([]);
+
+const existNext = computed(() => {
+    return nextTreeData.value && nextTreeData.value?.length > 0;
+});
+
+const select = ref<string | number>('');
+
+function clickSelectItem(label:string | number) {
+    select.value = label;
+    nextTreeData.value = props.treeData.find((item : Record<string, any>) => item.label === label)?.children || [];
+}
+
+</script>
+<style lang='less' scoped>
+.nav-menu{
+    border-right: 1px solid #f0f0f0;
+    .nav-item{
+        transition: all 0.3s;
+    }
+}
+</style>
